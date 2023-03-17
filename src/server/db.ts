2c3b92f56c8e393c23209df3d0f68865f4b1ad11
mitchellston <m.steenwijk@live.nl>
2023-03-17 21:28:23 +0100

    temp fix

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,16 +2,16 @@ import { PrismaClient } from "@prisma/client";
 import useAccelerate from "@prisma/extension-accelerate";
 import { env } from "../env/server.mjs";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
 export const prisma =
-  global.prisma ||
+  // global.prisma ||
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   }).$extends(useAccelerate);
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma.$extends(useAccelerate);
-}
+// if (env.NODE_ENV !== "production") {
+//   global.prisma = prisma;
+// }
